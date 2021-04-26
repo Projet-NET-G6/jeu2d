d984126e272dd6d6d3b1ffecfd6b0a151dc243ff
Nico-Goldorak <79379853+Nico-Goldorak@users.noreply.github.com>
2021-04-26 23:47:31 +0200

    e

---
+++ Assets/Scripts/Object/ActivationTrampoline.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationTrampoline : MonoBehaviour
+{
+    public float boostJump;
+
+    public Animator animator;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = (Vector2.up * boostJump);
+            animator.Play("ActivatedTrampoline");
+
+        }
+    }
+}
